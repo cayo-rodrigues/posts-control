ded32c9d6de635b15e2f0e1b7ace277c028d19c7
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-08-09 10:02:00 -0300

    fix: list user by id
    - list user handler had a boolean expression on line 16 that would always evaluate to false
    - list user handler missed an `await` clause when calling `getUserByIdService`
    - `getUserRepositories` had a commented block of code, and was returning `undefined` when query was successfull
    - `getUserRepositories` was deconstructing the query response as an object literal, but the response is an array

--- src/modules/handlers/User/listUsers.js
+++ src/modules/handlers/User/listUsers.js
@@ -2,20 +2,20 @@
 
 const httpStatusCodes = require('http-status-codes');
 const { httpErrorHandler } = require('../../common/handlers');
-const { 
-    getUserByIdService, 
-    getAllUsersService 
+const {
+    getUserByIdService,
+    getAllUsersService
 } = require('../../services');
 
 const listUserHandler = async (req, res, next) => {
-    try{
+    try {
         const {
             user_id
         } = req.query;
-        
-        const has_user_id = !!user_id && Number.isFinite(+user_id) && false;
 
-        const user_response = has_user_id && getUserByIdService({ user_id });
+        const has_user_id = !!user_id && Number.isFinite(+user_id);
+
+        const user_response = has_user_id && await getUserByIdService({ user_id });
 
         const users_response = !has_user_id && await getAllUsersService();
 
@@ -25,7 +25,7 @@ const listUserHandler = async (req, res, next) => {
         ];
 
         return res.status(httpStatusCodes.OK).send({users});
-    }catch(error){
+    } catch (error) {
         return httpErrorHandler({ req, res, error })
     }
 }
--- src/modules/repositories/User/getUserRepositories/getUserRepositories.js
+++ src/modules/repositories/User/getUserRepositories/getUserRepositories.js
@@ -1,4 +1,4 @@
-const { 
+const {
     client
 } = require('../../../common/handlers')
 
@@ -7,23 +7,19 @@ const getUserRepositories = async ({
     user_id
 } = {}) => {
 
-    const {
-        response
-    } = await client('users').where({ id: user_id })
+    const response = await client('users').where({ id: user_id })
 
     const has_response = Array.isArray(response) && response.length > 0;
 
-    if(!has_response){
+    if (!has_response) {
         return {
             users: []
         }
     }
 
-    // return {
-    //     users: response
-    // }
-    return undefined;
-
+    return {
+        users: response
+    }
 }
 
 module.exports = {
